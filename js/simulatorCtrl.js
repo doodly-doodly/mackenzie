768db47d528de65a63b211008c5d18794c310c86
Jebaraj <jjebaraj@adprod.bmc.com>
2016-02-21 00:30:41 +0530

    Joints and doodly moving

--- js/simulatorCtrl.js
+++ js/simulatorCtrl.js
@@ -1,32 +1,45 @@
-angular.module('doodly').controller('SimulatorCtrl', ['$scope', '$interval', 'uiGmapGoogleMapApi', 'GeolocationFactory', 'SimulatorFactory', function($scope, $interval, uiGmapGoogleMapApi, GeolocationFactory, SimulatorFactory){  
+angular.module('doodly').controller('SimulatorCtrl', ['$scope', '$interval', 'uiGmapGoogleMapApi', 'GeolocationFactory', 'SimulatorFactory', 'SimulatorService', function($scope, $interval, uiGmapGoogleMapApi, GeolocationFactory, SimulatorFactory, SimulatorService){  
 	console.log('Inside LandingCtrl');
 
-	var currentPositionIndex = 0;	
-	$scope.polylines = SimulatorFactory.getPolylineInputs();
-
-	$scope.doodlies = SimulatorFactory.getDoodlies();
+	var currentPositionIndex = 0;			
 
 	GeolocationFactory.getCurrentPosition().then(function (data) {
-        console.log(data);
-        $scope.map = { center: { latitude: data.lat, longitude: data.lng }, zoom: 15};        
-        $scope.markers = SimulatorFactory.getMarkers(data);
-    });
+        console.log(data); 
 
-    $scope.options = {
+        SimulatorService.getDoodlies().then(
+			function(result){
+				if(result){                            
+					console.log("Output Data :"+result);
+					$scope.doodlies = result.data;
+					$scope.map = { center: { latitude: data.lat, longitude: data.lng }, zoom: 15};   					  
+					$scope.allPolylines = SimulatorFactory.getPolylines(result.data);
+					$scope.allMarkers = SimulatorFactory.getMarkers(result.data);
+	          	}   
+			}
+		);	
+        
+    });
+	
+
+    /*$scope.options = {
     	icon: {
             url: 'images/doodly.png',
             animation: google.maps.Animation.DROP,
             scaledSize: new google.maps.Size(34, 44)
         }    	
-	};	
+	};	*/
 
     $interval(moveThePoints, 3000)
 
     function moveThePoints(){
-    	if($scope.markers && $scope.markers.length > 0){
-	    	console.log("Moving the points :"+currentPositionIndex);    	
-	    	$scope.markers[0].coords.latitude = $scope.polylines[currentPositionIndex].latitude;
-	    	$scope.markers[0].coords.longitude = $scope.polylines[currentPositionIndex].longitude;;
+    	if($scope.allMarkers && $scope.allMarkers.length > 0){
+	    	console.log("Moving the points :"+currentPositionIndex);  
+	    	angular.forEach($scope.allMarkers, function(marker){            
+	          if(marker.type == 'moving'){                
+	        	marker.coords.latitude = marker.polyLines[currentPositionIndex].latitude;
+	    		marker.coords.longitude = marker.polyLines[currentPositionIndex].longitude;                                    	
+	          }           
+        	});   		    	
 	    	currentPositionIndex++;
 	    	console.log("Moved the points :"+currentPositionIndex);
     	}
--- js/simulatorFactory.js
+++ js/simulatorFactory.js
@@ -1,54 +1,87 @@
-angular.module('doodly').factory('SimulatorFactory', ['$q', '$window', 'SimulatorService', function($q, $window, SimulatorService) {
+angular.module('doodly').factory('SimulatorFactory', ['$q', '$window',function($q, $window) {
 
     var simulatorFactory = this;
 
-    simulatorFactory.getPolylineInputs = function(geoJsonString){
-      if(!geoJsonString){
-        geoJsonString = 'awcnAeqtxMjBsFy@Yo@vAqAxDOh@G^APDP_G`CuBv@mDvAgDjAyAf@GD_AbBw@`BUdIIXWVo@FaD[eGa@_AGqC]WEr@kEvCeQ|@kG';
-      }
+    var stokeColors = ['#ff7373', '#bd00ff'];
 
-      var positions = polyline.decode(geoJsonString);
-      var points = [];
+    function getPositions(positions){
+      var polyLines = [];
       for(var i=0; i<positions.length; i++){
         var pos = positions[i];
         var point = {        
           "latitude" : pos[0],
           "longitude" : pos[1]
         }
-        points.push(point);
+        polyLines.push(point);
       }
-      return points;
+      return polyLines;
     }
 
-    simulatorFactory.getMarkers = function(data){
-      var markers = [
-        {     
-            id: 0,    
-            coords: { 
-              latitude: data.lat,
-              longitude: data.lng
-            }
-        },
-        {     
-              id: 1,    
-            coords: { 
-                latitude: data.lat + 0.001,
-                longitude: data.lng + 0.001
-            }
-        },
-        {     
-              id: 2,    
-            coords: { 
-                latitude: data.lat - 0.001,
-                longitude: data.lng - 0.001 
-            }
-        }     
-      ];
-      return markers;
+    function getStokeColor(counter){
+      if(counter%2 == 0){
+        return stokeColors[0];
+      }
+      return stokeColors[1];       
     }
 
-    simulatorFactory.getDoodlies = function(){
-      console.log("Output from webservice :"+SimulatorService.getDoodlies());
+    simulatorFactory.getPolylines = function(doodlies){
+      var allPolyLines = [];
+      if(doodlies){
+        var counter = 0;
+        angular.forEach(doodlies, function(doodly){     
+          var polyLine = {}       
+          if(doodly.type == 'moving'){                
+            var geoJsonString = doodly.pathPolyLine;
+            if(!geoJsonString){
+              geoJsonString = 'awcnAeqtxMjBsFy@Yo@vAqAxDOh@G^APDP_G`CuBv@mDvAgDjAyAf@GD_AbBw@`BUdIIXWVo@FaD[eGa@_AGqC]WEr@kEvCeQ|@kG';
+            }
+            polyLine.path = getPositions(polyline.decode(geoJsonString));
+            polyLine.stoke =  {
+              color : getStokeColor(counter)
+            }                  
+            //allPolyLines.push(getPositions(polyline.decode(geoJsonString)))                                 
+            allPolyLines.push(polyLine);
+            counter ++;
+          }           
+        });        
+      }
+      return allPolyLines;
     }
+
+
+    simulatorFactory.getPolylinesForDoodly = function(doodly){
+      var allPolyLines = [];                    
+      var geoJsonString = doodly.pathPolyLine;
+      if(!geoJsonString){
+        geoJsonString = 'awcnAeqtxMjBsFy@Yo@vAqAxDOh@G^APDP_G`CuBv@mDvAgDjAyAf@GD_AbBw@`BUdIIXWVo@FaD[eGa@_AGqC]WEr@kEvCeQ|@kG';
+      }
+      return getPositions(polyline.decode(geoJsonString));                           
+    }
+
+    simulatorFactory.getMarkers = function(doodlies){
+      var allMarkers = [];
+      if(doodlies){
+        angular.forEach(doodlies, function(doodly){  
+          var marker = {};          
+          marker.id = doodly.id;
+          marker.type = doodly.type;
+          marker.coords = {
+            latitude: doodly.currentPosition.lat,
+            longitude: doodly.currentPosition.lng
+          }
+          marker.option = {
+            icon : {
+              url: (doodly.type == 'moving'? 'images/doodly.png' : 'images/joint.png'),
+              animation: google.maps.Animation.DROP,
+              scaledSize: new google.maps.Size(34, 44)
+            }            
+          }
+          marker.polyLines = simulatorFactory.getPolylinesForDoodly(doodly);
+          allMarkers.push(marker);         
+        });        
+      }
+      return allMarkers;
+    }
+   
     return simulatorFactory;
 }]);
