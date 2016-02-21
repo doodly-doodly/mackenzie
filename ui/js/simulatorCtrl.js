5d752a465a7a0f953fd87f1b97ec2cf270edbe11
Jebaraj <jjebaraj@adprod.bmc.com>
2016-02-21 22:45:50 +0530

    Add package window

--- ui/js/simulatorCtrl.js
+++ ui/js/simulatorCtrl.js
@@ -18,46 +18,7 @@ angular.module('doodly').controller('SimulatorCtrl', ['$scope', '$interval', '$m
 					 zoom: 15, 
 					 events: {
 					 	click: function (maps, eventName, args) {
-					 		var e = args[0];
-                			var lat = e.latLng.lat(),lon = e.latLng.lng();
-                			SimulatorFactory.setClickedLocation(lat, lon);
-
-					 		console.log("lat :"+lat+" lon :"+lon);
-					 		var marker = {
-			                    id: Date.now(),
-			                    type : 'MOVING',
-			                    coords: {
-			                        latitude: lat,
-			                        longitude: lon
-			                    },
-			                    option : {
-            						icon : {
-              							url: 'images/doodlynew.png',
-              							animation: google.maps.Animation.DROP,
-              							scaledSize: new google.maps.Size(34, 34)
-            						}            
-          						}
-                			};
-                			var modalInstance = $modal.open({
-                				templateUrl: 'partials/registration.html',
-                				controller: 'SimulatorModalCtrl',
-                				backdrop: 'static',
-                				keyboard: false
-            				});         
-            				modalInstance.result.then(function (userSelection) {              
-                				console.log('userSelection :' + userSelection);
-                				if(userSelection){     
-                					if(userSelection.userType == 0){
-                						marker.type = 'package';  	                				
-	                					marker.option.icon.url = 'images/package.png';	                						                    				
-                					} else if(userSelection.userType == 2){
-                						marker.type = 'joint';  	                				
-	                					marker.option.icon.url = 'images/joint.png';
-                					}               					
-                    				$scope.allMarkers.push(marker);
-                				}                
-            				}, function () {                			
-            				});                			
+					 		  handleMapClick(args);
 					 	}
 					 }
 					};   					  					
@@ -66,6 +27,46 @@ angular.module('doodly').controller('SimulatorCtrl', ['$scope', '$interval', '$m
 	          	}   
 			}
 		);	
+
+	function handleMapClick(args){
+		var e = args[0];
+		var lat = e.latLng.lat(),lon = e.latLng.lng();
+		SimulatorFactory.setClickedLocation(lat, lon);
+
+ 		console.log("lat :"+lat+" lon :"+lon);
+ 		var marker = { id: Date.now(), type : 'MOVING', coords: {
+                latitude: lat,
+                longitude: lon
+            },
+            option : {
+				icon : {
+					url: 'images/doodlynew.png',
+					animation: google.maps.Animation.DROP,
+					scaledSize: new google.maps.Size(34, 34)
+				}            
+			}
+		};
+		var modalInstance = $modal.open({
+			templateUrl: 'partials/registration.html',
+			controller: 'SimulatorModalCtrl',
+			backdrop: 'static',
+			keyboard: false
+		});         
+		modalInstance.result.then(function (userSelection) {              
+			console.log('userSelection :' + userSelection);
+			if(userSelection){     
+				if(userSelection.userType == 0){
+					marker.type = 'package';  	                				
+					marker.option.icon.url = 'images/package.png';	                						                    				
+				} else if(userSelection.userType == 2){
+					marker.type = 'joint';  	                				
+					marker.option.icon.url = 'images/joint.png';
+				}               					
+				$scope.allMarkers.push(marker);
+			}                
+		}, function () {                			
+		});     
+	}
 
     $interval(moveThePoints, 3000)
 
@@ -115,16 +116,24 @@ angular.module('doodly').controller('SimulatorModalCtrl', ['$scope', '$modalInst
     	});               
 
         $scope.userSelection.src = clickedLocation.locationName;
+        $scope.userSelection.srcLat = clickedLocation.lat;
+        $scope.userSelection.srcLng = clickedLocation.lng;
         var events = {
     		places_changed: function (searchBox) {
-        		var place = searchBox.getPlaces();
-        		if (!place || place == 'undefined' || place.length == 0) {
-            		console.log('no place data :(');
-            		return;
-        		}
-    		}
+        		var places = searchBox.getPlaces();
+        		if(places && places.length > 0){         			
+        			$scope.userSelection.dest = places[0].formatted_address;			
+        			$scope.userSelection.destLat = places[0].geometry.location.lat();
+        			$scope.userSelection.destLng = places[0].geometry.location.lng();
+        		}        			
+        	}
     	}
+    	
         $scope.searchbox = { template: 'partials/searchTemplate.html', events: events, parentdiv : 'destDiv' };
+
+        $scope.newMap = { center: {latitude: 12.970994, longitude: 77.604815},
+			zoom: 15 			
+		};
 
         $scope.applySelectedInputs = function () {          	
         	$modalInstance.close($scope.userSelection);
@@ -134,12 +143,46 @@ angular.module('doodly').controller('SimulatorModalCtrl', ['$scope', '$modalInst
             $modalInstance.dismiss('cancel');
         };
 
+      /*  function keyPress(){
+        	var autocompleteService = new google.maps.places.AutocompleteService();
+        	autocompleteService.getPlacePredictions({input: 'Ban'}, function(predictions, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    console.log(predictions);
+                }
+            });
+        }
+
         google.maps.event.addDomListener(window, 'load', initSearch);
+
+        var autocompleteService = new google.maps.places.AutocompleteService();
+       // var autocomplete = new google.maps.places.Autocomplete(input);
+
+		$scope.getPredictions = function(query){			
+
+			if(query && query.length > 3){
+				var searchObj = {
+	        		input: query
+    			};
+
+	    		autocompleteService.getPlacePredictions(searchObj, function(predictions, status) {
+	    			console.log("Place name is:"+predictions[0].getPlace());
+	            	$scope.response = predictions;                
+	    		});
+			}			
+		}
+        
 
         function initSearch() {
 			var input = document.getElementById('searchTextField');
+			input.on('keydown', function() {
+    			keyPress();
+			});
 			var autocomplete = new google.maps.places.Autocomplete(input);
-		}
+			google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    			//fillInAddress();
+    			console.log("Inside");
+  			});
+		}*/
 
 
 }]);
