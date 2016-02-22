7a4ee275143ca6936733f5a7d1945ed191783f28
unknown <spichapp@BMC-4HQJMV1.adprod.bmc.com>
2016-02-22 03:59:50 +0000

    Request delivery API change

--- api/routes/RequestDelivery.js
+++ api/routes/RequestDelivery.js
@@ -1,22 +1,27 @@
 /*
  * Project Doodly GetDoodlies service
  */
-var esmodule = require("../../esmodule"); 
+var esmodule = require("../../esmodule");
 exports.requestDeliveryImpl = function (req, res)
 {
     var data = req.body;
-    
+
     //send delivery request details to magna
-    //expected reply is which joint is going to handle the delivery 
-    //send back joint details to client, where client animates joint icon by showing some movements or tool tip 
+    //expected reply is which joint is going to handle the delivery
+    //send back joint details to client, where client animates joint icon by showing some movements or tool tip
     //after some time package icon displayed in requeted location will hidden indicating joint picked up it
-    //res.writeHead(200, "OK", {'Content-Type': 'text/html'});    
+    //res.writeHead(200, "OK", {'Content-Type': 'text/html'});
     //res.end("Fasoos");
-    esmodule.createBooking(data, function(packageId, jointId){
-            res.writeHead(200, "OK", {'Content-Type': 'text/html'});    
-            res.end(jointId);
+    //esmodule.createBooking(data, function(packageId, jointId, doodlyId, polyLine){
+    esmodule.createBooking(data, function(status, result){
+    	if(status == "ok"){
+    		res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+    		res.end(JSON.stringify(result));
+    	}else{
+    		res.writeHead(100, "error", {'Content-Type': 'text/html'});
+    		res.end(result);
+    	}
 
     });
 
-    //res.end(JSON.stringify(data));
 };
